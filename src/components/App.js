fb51e575575c7db093d6de661528aa5a14f01656
Sourav Bose <hudson.sourav@gmail.com>
2021-04-30 11:00:05 +0530

    saving to github

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Header from './Header'
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+    <Header/>
+     </div>
+    
   )
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+             <h1 data-ns-test="project-name">Covid-Tracking-App</h1>
+           <p data-ns-test="project-description">I want to make covid tracking app to help the society to aware of</p>
+        
+        </div>
+    )
+}
